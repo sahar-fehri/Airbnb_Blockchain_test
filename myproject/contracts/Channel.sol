951910789d72134805897f2a6c2a4fd20b491a15
sahar <fehrisahar14@gmail.com>
2018-02-10 17:05:33 +0100

    fixed test function to validate a signature from a signed msg

--- myproject/contracts/Channel.sol
+++ myproject/contracts/Channel.sol
@@ -50,7 +50,7 @@ contract Channel {
 	
 
 
-	function verify(bytes32 hash, uint8 v, bytes32 r, bytes32 s) internal constant returns(address) {
+	function verify(bytes32 hash, uint8 v, bytes32 r, bytes32 s)  constant returns(address) {
 
         bytes memory prefix = "\x19Ethereum Signed Message:\n32";
         bytes32 prefixedHash = keccak256(prefix, hash);
